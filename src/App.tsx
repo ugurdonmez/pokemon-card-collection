8b0adae048f9d4a1b0242a2504c43c8b09256021
Donmez, Ugur <ugurdonmez@protonmail.com>
2025-02-02 20:23:02 +0100

    show cards to the screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from 'antd'
+import CardList from './components/CardList'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="App">
-        <Button type="primary">Button</Button>
+    <div className="App">
+        <h1>Pokémon Card Collection</h1>
+        <CardList />
       </div>
-
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
