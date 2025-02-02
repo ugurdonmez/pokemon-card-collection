b62901ce5dc68bb6effa6f6fdc0a78be6d4bd4e8
Donmez, Ugur <ugurdonmez@protonmail.com>
2025-02-02 20:00:57 +0100

    add antd

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from 'antd'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
