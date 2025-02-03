2005d2f7188ca12af491c185e448e7699f17727a
Donmez, Ugur <ugurdonmez@protonmail.com>
2025-02-04 00:45:28 +0100

    fix path for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pokemon-card-collection/',
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
