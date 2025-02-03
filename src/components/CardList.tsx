86f229ff47c4454aa7c6624b5ed2f3d804e0d8de
Donmez, Ugur <ugurdonmez@protonmail.com>
2025-02-03 15:36:14 +0100

    improve ui

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,36 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import { Col, Row, Spin } from 'antd';
+import { List, Spin } from 'antd';
 import CardItem from '@components/CardItem';
 import ashCollection from '../data/ash_collection.json';
 import { PokemonCard } from '@types';
 
-
 const CardList: React.FC = () => {
-    const [cards, setCards] = useState<PokemonCard[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
+  const [cards, setCards] = useState<PokemonCard[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        const fetchCards = async () => {
-            setCards(ashCollection as unknown as PokemonCard[]);
-            setLoading(false);
-        };
+  useEffect(() => {
+    setCards(ashCollection as unknown as PokemonCard[]);
+    setLoading(false);
+  }, []);
 
-        fetchCards();
-    }, []);
+  if (loading) {
+    return <Spin size="large" />;
+  }
 
-    if (loading) {
-        return <Spin size="large" />;
-    }
-
-    return (
-        <Row gutter={16}>
-            {cards.map((card) => (
-                <Col span={8} key={card.id}>
-                    <CardItem pokemonCard={card} />
-                </Col>
-            ))}
-        </Row>
-    );
+  return (
+    <List
+      grid={{
+        gutter: 16,
+        xs: 1,
+        sm: 2,
+        md: 3,
+        lg: 4,
+        xl: 4,
+        xxl: 4,
+      }}
+      dataSource={cards}
+      renderItem={(card: PokemonCard) => (
+        <List.Item key={card.id}>
+          <CardItem pokemonCard={card} />
+        </List.Item>
+      )}
+    />
+  );
 };
 
 export default CardList;
