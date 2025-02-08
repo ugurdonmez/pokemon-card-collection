a64a563a87cf17634ae29624f0ad379665d2bc43
Donmez, Ugur <ugurdonmez@protonmail.com>
2025-02-08 18:42:45 +0100

    fix: card item summary

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -46,51 +46,6 @@ const CardItem: React.FC<CardItemProps> = ({ pokemonCard }) => {
                         </div>
                     )}
 
-                    {/* Evolution Info */}
-                    {pokemonCard.evolvesFrom && <p>Evolves from: <strong>{pokemonCard.evolvesFrom}</strong></p>}
-                    {pokemonCard.evolvesTo && pokemonCard.evolvesTo.length > 0 && (
-                        <p>Evolves to: {pokemonCard.evolvesTo.join(', ')}</p>
-                    )}
-
-                    {/* Attacks */}
-                    {pokemonCard.attacks && pokemonCard.attacks.length > 0 && (
-                        <div className="card-attacks">
-                            <h4>Attacks:</h4>
-                            {pokemonCard.attacks.map((attack) => (
-                                <Tooltip key={attack.name} title={attack.text}>
-                                    <p><strong>{attack.name}</strong> - {attack.damage}</p>
-                                </Tooltip>
-                            ))}
-                        </div>
-                    )}
-
-                    {/* Weaknesses & Resistances */}
-                    <div className="card-stats">
-                        {pokemonCard.weaknesses && (
-                            <div className="weakness-section">
-                                <h4>Weaknesses:</h4>
-                                {pokemonCard.weaknesses.map((weakness) => (
-                                    <Tag key={weakness.type} color="volcano">
-                                        {weakness.type} {weakness.value}
-                                    </Tag>
-                                ))}
-                            </div>
-                        )}
-
-                        {pokemonCard.resistances && (
-                            <div className="resistance-section">
-                                <h4>Resistances:</h4>
-                                {pokemonCard.resistances.map((resistance) => (
-                                    <Tag key={resistance.type} color="green">
-                                        {resistance.type} {resistance.value}
-                                    </Tag>
-                                ))}
-                            </div>
-                        )}
-                    </div>
-
-                    {/* Flavor Text */}
-                    {pokemonCard.flavorText && <p className="flavor-text">"{pokemonCard.flavorText}"</p>}
                 </div>
             </Card>
 
